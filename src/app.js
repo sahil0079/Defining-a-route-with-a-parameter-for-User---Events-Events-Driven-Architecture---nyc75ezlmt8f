69443bb01a5a11ad6d7b5579c23110abbe192f24
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-12-09 22:48:14 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
+app.get('/user/:userId', (req, res) => {
   //Write a code here for endpoint /user/:userId and also print parameter in json form
-});
+
+  const userId = req.params.userId;
+
+  if(!userId || isNaN(userId)){
+    res.status(400).json({
+      error: 'Invalid userId'
+    })
+}else{
+    res.status(200).json({userId)
+  }
+  );
 
 module.exports = app;
